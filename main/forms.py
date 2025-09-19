66e0697ca9b69dc37e3983c84e867c74bc3091c0
pavelplus <pavel.irf@gmail.com>
2025-09-19 18:09:36 +0300

    experiments with libs

--- main/forms.py
+++ main/forms.py
@@ -1,7 +1,10 @@
 from django.forms import ModelForm
+from django.forms.renderers import DjangoTemplates
 
 from .models import Person
 
+
+renderer = DjangoTemplates
 
 class PersonForm(ModelForm):
     class Meta:
--- sandbox/settings.py
+++ sandbox/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'django_bootstrap5',
+    'widget_tweaks',
+
     'main',
 ]
 
